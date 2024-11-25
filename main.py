d0a5c37b597f67af2abff679ff470f59bcf968bc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-24 22:01:19 -0300

    feat: create inputs

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+import streamlit as st
+
+st.title("Calculadora de IMC")
+
+weight = st.number_input("Digite o seu peso em Kg")
+height_scale = st.radio("Selecione a opção de altura", ("centímetros", "metros"))
+height = st.number_input(f"Digite a altura em {height_scale}")
