59138b52ee430506f02d49cb140ee8f6464d4fb1
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-24 22:52:08 -0300

    feat: classify imc

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ if st.button("Calcular IMC"):
                 imc = weight / ((height / 100) ** 2)
             case "metros":
                 imc = weight / height**2
+
         st.text("O seu IMC é {:.2f}".format(imc))
+
+        if imc < 16:
+            st.error("Extremamente abaixo do peso")
+        elif imc < 18.5:
+            st.warning("Abaixo do peso")
+        elif imc < 25:
+            st.success("Saudável")
+        elif imc < 30:
+            st.warning("Acima do peso")
+        else:
+            st.error("Extremanete acima do peso")
     except ZeroDivisionError:
         st.error("Impossível divisão por zero.")
     except ValueError:
