53e05fa96de1b38f579bcd4ca5be7e87ffe4a006
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-24 22:38:25 -0300

    feat: calc imc

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ st.title("Calculadora de IMC")
 weight = st.number_input("Digite o seu peso em Kg")
 height_scale = st.radio("Selecione a opção de altura", ("centímetros", "metros"))
 height = st.number_input(f"Digite a altura em {height_scale}")
+
+if st.button("Calcular IMC"):
+    try:
+        match height_scale:
+            case "centímetros":
+                imc = weight / ((height / 100) ** 2)
+            case "metros":
+                imc = weight / height**2
+        st.text("O seu IMC é {:.2f}".format(imc))
+    except ZeroDivisionError:
+        st.error("Impossível divisão por zero.")
+    except ValueError:
+        st.error("Valor inválido.")
